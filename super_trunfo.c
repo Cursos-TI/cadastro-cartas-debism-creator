9c6f5b487943fbd73b072586fcc6b9172d7e393c
debism-creator <debis_m@hotmail.com>
2025-11-16 00:03:58 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,11 +1,43 @@
 #include <stdio.h>
 
 int main () {
-    printf("Carta:"\n)
-char Estado
-printf("Estado:"\n);
-scanf( "%s, &Estado);
-  printf("Estado: %s\n", Estado);
-return 0;
+    printf("Carta 1\n");
+char estado1;
+printf("Estado: ");
+scanf( "%s", &estado1);
+char cidade1;
+printf("Cidade: ");
+scanf( "%s", &cidade1);
+int populacao1;
+printf("População: ");
+scanf( "%i", &populacao1);
+float area1;
+printf("Área (em km²): ");
+scanf( "%f", &area1);
+float PIB1;
+printf("PIB: ");
+scanf( "%f", &PIB1);
+int turistico1;
+printf("Número de Pontos Turísticos: ");
+scanf( "%i", &turistico1);
 
-}
+    printf("\nCarta 2\n");
+char estado2;
+printf("Estado: ");
+scanf( "%s", &estado2);
+char cidade2;
+printf("Cidade: ");
+scanf( "%s", &cidade2);
+int populacao2;
+printf("População: ");
+scanf( "%i", &populacao2);
+float area2;
+printf("Área (em km²): ");
+scanf( "%f", &area2);
+float PIB2;
+printf("PIB: ");
+scanf( "%f", &PIB2);
+int turistico2;
+printf("Número de Pontos Turísticos: ");
+scanf( "%i", &turistico2);
+return 0;
